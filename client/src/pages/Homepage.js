a15ee51629767db3a42dbf670cfadaba6f076915
mesahoang <me.sa.hoang@gmail.com>
2021-05-11 21:14:33 -0500

    added grid and homepage.js elements

---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Col, Row, Container } from "../components/Grid";
+import Jumbotron from "../components/Jumbotron";
+import Nav from "../components/Nav";
+
+
+
+export default Home;
