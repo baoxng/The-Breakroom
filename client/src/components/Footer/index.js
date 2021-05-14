16fc11d0af9f153077edfc757434fea345b8b51a
mesahoang <me.sa.hoang@gmail.com>
2021-05-13 19:21:49 -0500

    added src component folders

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Created by Melissa Hoang, Bao Xiong, Josie Collins</span>
+    </footer>
+  );
+}
+
+export default Footer;
