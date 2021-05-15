d413b1bdf661400d5dcae0c5bc1c722798b0521c
Bao Xiong <bao.xiong2011@gmail.com>
2021-05-15 10:21:10 -0500

    Added session on server.js.

--- server.js
+++ server.js
@@ -11,10 +11,26 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/build")); 
 }
+
+//Start session
+app.use(require('express-session')({
+  secret: 'This is a secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+  },
+  store: store,
+  // Boilerplate options, see:
+  // * https://www.npmjs.com/package/express-session#resave
+  // * https://www.npmjs.com/package/express-session#saveuninitialized
+  resave: true,
+  saveUninitialized: true
+}));
+
 // Add routes, both API and view
 app.use(routes);
 
