5ca0765152648eca8ec01c07ab9da304290c8447
mesahoang <me.sa.hoang@gmail.com>
2021-05-20 20:20:49 -0500

    added comet chat

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Jumbotron() {
-
+    return (
+        <div class="jumbotron-row">
+            <h1>The Breakroom</h1>
+            <p>let's play</p>
+        </div>
+    );
 }
 
 export default Jumbotron;
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Col, Row, Container } from "../components/Grid";
 import TimerApp from "../components/Timer/App";
-// import Jumbotron from "../components/Jumbotron";
+import Jumbotron from "../components/Jumbotron";
 // import Nav from "../components/Nav";
 
 const Homepage = () => {
     return (
         <Container fluid>
             <Row>
+                <Jumbotron />
                 <p>
                     <TimerApp />
                 </p>
