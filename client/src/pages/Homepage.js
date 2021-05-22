b6e81805d01b8ed4b80fe8e19151fc4d92528401
mesahoang <me.sa.hoang@gmail.com>
2021-05-22 13:22:50 -0500

    updates

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,8 +1,7 @@
 import React from "react";
-// import ChatApp from "../components/Chat/app";
 import { Row, Container } from "../components/Grid";
 import TimerApp from "../components/Timer/App";
-import ChatApp from "../components/Chat/App";
+import ChatApp from "../components/Chat/app";
 
 const Homepage = () => {
     return (
